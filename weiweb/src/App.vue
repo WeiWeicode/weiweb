b91ddb3751f320a60a3d145445e6ae343ab2171a
Jia-wei Jiang <asdf0359@gmail.com>
2023-10-07 17:17:39 +0800

    first commit

---
+++ weiweb/src/App.vue
@@ -0,0 +1,192 @@
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+
+
+
+  <!-- <RouterView /> -->
+
+
+  <div>
+    <el-container>
+      <el-header>
+
+
+        <h1>Wei</h1>
+
+
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-scrollbar>
+            <el-menu router :default-openeds="['1', '3']">
+              <el-sub-menu index="1">
+                <template #title>
+                  <el-icon>
+                    <message />
+                  </el-icon>Navigator One
+                </template>
+                <el-menu-item-group>
+                  <template #title>Group 1</template>
+                  <el-menu-item index="1-1">Option 1</el-menu-item>
+                  <!-- 到首頁 -->
+                  <el-menu-item index="/">
+                    <!-- <RouterLink to="/">Home</RouterLink> -->
+                    Home
+                  </el-menu-item>
+                  <!-- about -->
+                  <el-menu-item index="/about">
+                    <!-- <RouterLink to="/about">About</RouterLink> -->
+                    About
+                  </el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group 2">
+                  <el-menu-item index="1-3">Option 3</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="1-4">
+                  <template #title>Option4</template>
+                  <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
+                </el-sub-menu>
+              </el-sub-menu>
+              <el-sub-menu index="2">
+                <template #title>
+                  <el-icon><icon-menu /></el-icon>Navigator Two
+                </template>
+                <el-menu-item-group>
+                  <template #title>Group 1</template>
+                  <el-menu-item index="2-1">Option 1</el-menu-item>
+                  <el-menu-item index="2-2">Option 2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group 2">
+                  <el-menu-item index="2-3">Option 3</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="2-4">
+                  <template #title>Option 4</template>
+                  <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
+                </el-sub-menu>
+              </el-sub-menu>
+              <el-sub-menu index="3">
+                <template #title>
+                  <el-icon>
+                    <setting />
+                  </el-icon>Navigator Three
+                </template>
+                <el-menu-item-group>
+                  <template #title>Group 1</template>
+                  <el-menu-item index="3-1">Option 1</el-menu-item>
+                  <el-menu-item index="3-2">Option 2</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group 2">
+                  <el-menu-item index="3-3">Option 3</el-menu-item>
+                </el-menu-item-group>
+                <el-sub-menu index="3-4">
+                  <template #title>Option 4</template>
+                  <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+                </el-sub-menu>
+              </el-sub-menu>
+            </el-menu>
+          </el-scrollbar>
+        </el-aside>
+        <!-- <el-aside>
+          <nav>
+            <RouterLink to="/">Home</RouterLink>
+          </nav>
+          <nav>
+            <RouterLink to="/about">About</RouterLink>
+          </nav>
+        </el-aside> -->
+        <el-main style="width: 800px;">
+          <RouterView />
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+
+const test = () => {
+  console.log('test')
+}
+
+</script>
+
+
+<!-- 
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  /* margin: 0 auto 2rem; */
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style> -->
---
+++ weiweb/src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>And the url is /about</p>
+    <el-button>Default</el-button>
+  </div>  
+</template>
+
+<script>
+
+
+
+</script>
+
+<!-- <style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    /* display: flex; */
+    align-items: center;
+  }
+}
+</style> -->
