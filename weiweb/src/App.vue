579e647dcd01787c887e109efcdd43ee9271064e
Wei <110208335+WeiWeicode@users.noreply.github.com>
2023-10-27 16:48:41 +0800

    Update App.vue

--- weiweb/src/App.vue
+++ weiweb/src/App.vue
@@ -12,181 +12,66 @@
     </div>
   </header> -->
 
-
-
   <!-- <RouterView /> -->
 
-
   <div>
+    <el-header> </el-header>
+
     <el-container>
-      <el-header>
+      <el-aside width="200px">
+        <el-scrollbar>
+          <el-menu
+            default-active="2"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+            router
+          >
+            
 
+            <!-- 用script產生目錄 -->
+            <el-sub-menu v-for="item in menu" :key="item.id" :index="item.path">
+              <template #title>
+                <el-icon>
+                  <Edit />
+                </el-icon>
+                <span>{{ item.name }}</span>
+              </template>
+              <el-menu-item v-for="app in item.app" :key="app.id" :index="app.path">
+                <span>{{ app.name }}</span>
+              </el-menu-item>
+              
+            </el-sub-menu>
+          </el-menu>
+        </el-scrollbar>
+      </el-aside>
 
-        <h1>Wei</h1>
-
-
-      </el-header>
-      <el-container>
-        <el-aside width="200px">
-          <el-scrollbar>
-            <el-menu router :default-openeds="['1', '3']">
-              <el-sub-menu index="1">
-                <template #title>
-                  <el-icon>
-                    <message />
-                  </el-icon>Navigator One
-                </template>
-                <el-menu-item-group>
-                  <template #title>Group 1</template>
-                  <el-menu-item index="1-1">Option 1</el-menu-item>
-                  <!-- 到首頁 -->
-                  <el-menu-item index="/">
-                    <!-- <RouterLink to="/">Home</RouterLink> -->
-                    Home
-                  </el-menu-item>
-                  <!-- about -->
-                  <el-menu-item index="/about">
-                    <!-- <RouterLink to="/about">About</RouterLink> -->
-                    About
-                  </el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="Group 2">
-                  <el-menu-item index="1-3">Option 3</el-menu-item>
-                </el-menu-item-group>
-                <el-sub-menu index="1-4">
-                  <template #title>Option4</template>
-                  <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
-                </el-sub-menu>
-              </el-sub-menu>
-              <el-sub-menu index="2">
-                <template #title>
-                  <el-icon><icon-menu /></el-icon>Navigator Two
-                </template>
-                <el-menu-item-group>
-                  <template #title>Group 1</template>
-                  <el-menu-item index="2-1">Option 1</el-menu-item>
-                  <el-menu-item index="2-2">Option 2</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="Group 2">
-                  <el-menu-item index="2-3">Option 3</el-menu-item>
-                </el-menu-item-group>
-                <el-sub-menu index="2-4">
-                  <template #title>Option 4</template>
-                  <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
-                </el-sub-menu>
-              </el-sub-menu>
-              <el-sub-menu index="3">
-                <template #title>
-                  <el-icon>
-                    <setting />
-                  </el-icon>Navigator Three
-                </template>
-                <el-menu-item-group>
-                  <template #title>Group 1</template>
-                  <el-menu-item index="3-1">Option 1</el-menu-item>
-                  <el-menu-item index="3-2">Option 2</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="Group 2">
-                  <el-menu-item index="3-3">Option 3</el-menu-item>
-                </el-menu-item-group>
-                <el-sub-menu index="3-4">
-                  <template #title>Option 4</template>
-                  <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
-                </el-sub-menu>
-              </el-sub-menu>
-            </el-menu>
-          </el-scrollbar>
-        </el-aside>
-        <!-- <el-aside>
-          <nav>
-            <RouterLink to="/">Home</RouterLink>
-          </nav>
-          <nav>
-            <RouterLink to="/about">About</RouterLink>
-          </nav>
-        </el-aside> -->
-        <el-main style="width: 800px;">
-          <RouterView />
-        </el-main>
-      </el-container>
+      <el-main style="width: 800px">
+        <RouterView />
+      </el-main>
     </el-container>
   </div>
 </template>
 
+<script lang="ts" setup>
+import { Document, Menu as IconMenu, Location, Setting } from "@element-plus/icons-vue";
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath);
+};
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath);
+};
 
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-
-
-const test = () => {
-  console.log('test')
-}
-
+// menu 首頁 關於 APP
+const menu = [
+  {
+    id: "1",
+    name: "首頁",
+    path: "/",
+    icon: "Document",
+    app: [{ id: "1-1", name: "1-1", path: "/" }],
+  },
+  { id: "2", name: "關於", path: "/about", icon: "Location",app: [{ id: "2-1", name: "關於", path: "/about" }],},
+  { id: "3", name: "APP", path: "/app", icon: "Setting" },
+];
 </script>
-
-
-<!-- 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  /* margin: 0 auto 2rem; */
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style> -->
