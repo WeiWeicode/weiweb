905273625ac1485f87f68328272993d5a67578a6
Wei <110208335+WeiWeicode@users.noreply.github.com>
2023-10-27 16:50:21 +0800

    Update main.js

--- weiweb/src/main.js
+++ weiweb/src/main.js
@@ -1,4 +1,4 @@
-// import './assets/main.css'
+import './assets/main.css'
 
 
 import { createApp } from 'vue'
@@ -9,9 +9,12 @@ import router from './router'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
-
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
 app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
